55f717eb1da03f51f6965729240a6ad1609cb628
minidas <NULL>
2025-10-07 18:51:20 +0800

    feat: FH model

--- fermion.py
+++ fermion.py
@@ -7,7 +7,7 @@ from scipy.sparse.linalg import expm_multiply
 from qiskit.quantum_info import Statevector
 import numpy as np
 import matplotlib.pyplot as plt
-
+from tqdm import tqdm
 class Fermion_Model:
     def __init__(self,tunneling = 1.0,coulomb = 4.0):
         self.tunneling = tunneling
@@ -17,7 +17,8 @@ class Fermion_Model:
     def get_target_hamiltonian(self,tunneling = 1.0,coulomb = 4.0):
         # define the target Hamiltonian
         
-        hamiltonian = fermi_hubbard(2, 2, tunneling, coulomb, periodic=False)
+        hamiltonian = fermi_hubbard(1, 8, tunneling, coulomb, periodic=False)
+        # print(hamiltonian)
         H_qubit = jordan_wigner(hamiltonian)
         return H_qubit
     def evolve_state(self, psi0,t_total, hamiltonian):
@@ -28,24 +29,22 @@ class Fermion_Model:
     
 class Analog_Fermion(Fermion_Model):
     def __init__(self,model):
-        self.H=self.get_target_hamiltonian(model.tunneling+0.1, model.coulomb)
+        self.H=self.get_target_hamiltonian(model.tunneling, model.coulomb+0.01)
 
 if __name__ == "__main__":
     exact_model = Fermion_Model(tunneling=1.0, coulomb=4.0)
     analog_model = Analog_Fermion(exact_model)
 
-    initial_state = Statevector.from_label('01100110').data  # Example initial state
+    initial_state = Statevector.from_label('0000111100001111').data  # Example initial state
     time=np.linspace(0,10,101)
     exact_state=initial_state
     analog_state=initial_state
     time_step=0.1
     error=[]
-    for _ in time:
+    for _ in tqdm(time):
         analog_state = analog_model.evolve_state(exact_state, time_step, analog_model.H)
         exact_state = exact_model.evolve_state(exact_state, time_step, exact_model.H)
         error.append(np.linalg.norm(exact_state - analog_state))
     
     plt.plot(time, error)
     plt.savefig('Figures/fermion_error.pdf', bbox_inches='tight',dpi=600)
-                                                  
-        
