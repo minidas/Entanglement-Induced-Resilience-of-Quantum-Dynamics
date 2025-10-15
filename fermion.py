dee81ff3f6dfd632593bfaea525a2e5a033bc3f5
minidas <NULL>
2025-10-15 14:13:40 +0800

    test

--- fermion.py
+++ fermion.py
@@ -17,7 +17,7 @@ class Fermion_Model:
     def get_target_hamiltonian(self,tunneling = 1.0,coulomb = 4.0):
         # define the target Hamiltonian
         
-        hamiltonian = fermi_hubbard(1, 8, tunneling, coulomb, periodic=False)
+        hamiltonian = fermi_hubbard(1, 8, tunneling, coulomb)
         # print(hamiltonian)
         H_qubit = jordan_wigner(hamiltonian)
         return H_qubit
@@ -29,14 +29,14 @@ class Fermion_Model:
     
 class Analog_Fermion(Fermion_Model):
     def __init__(self,model):
-        hamiltonian = fermi_hubbard(1, 8, model.tunneling, model.coulomb+0.01,periodic=False)
+        hamiltonian = fermi_hubbard(1, 8, model.tunneling, model.coulomb+0.01)
         self.H=jordan_wigner(hamiltonian)
 
 if __name__ == "__main__":
     exact_model = Fermion_Model(tunneling=1.0, coulomb=0)
     analog_model = Analog_Fermion(exact_model)
 
-    initial_state = Statevector.from_label('1111000000000000').data  # Example initial state
+    initial_state = Statevector.from_label('1111111100000000').data  # Example initial state
     time=np.linspace(0,10,101)
     exact_state=initial_state
     analog_state=initial_state
