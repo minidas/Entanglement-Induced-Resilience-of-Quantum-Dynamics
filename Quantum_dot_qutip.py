5ad3e3f8045871edee889336bd583bca6f002133
minidas <caoyue3004@gmail.com>
2025-11-15 11:18:12 +0800

    feat: RCPs

--- Quantum_dot_qutip.py
+++ Quantum_dot_qutip.py
@@ -1,7 +1,7 @@
 from qiskit.quantum_info import SparsePauliOp, Statevector, Pauli
 import numpy as np
 import matplotlib.pyplot as plt
-from math import sqrt
+from math import sqrt,sin, cos, tan, pi
 import qutip as qt
 
 final_time=6
@@ -23,11 +23,14 @@ class Quantum_dot:
         self.N = N
         self.H = self.get_hamiltonian(Omega, N)
 
+    def Omega(self,t,a,phi,T):
+        return sin(pi*t/T)*(a[0]+np.sum([a[k]*sin(2*pi*k*t/T+phi[k]) for k in range(1,num_rand_PS+1)]))
+
     def get_hamiltonian(self, Omega, N):
         IX = qt.tensor(
             *[qt.sigmax() if i == 0 else qt.qeye(2) for i in range(N)]
         )
-        return Omega * 0.5* IX
+        return 0.5* IX*qt.coefficient(self.Omega)
     
 from random_PS import *
 num_rand_PS=2**(N-2)#number of random Pauli strings
@@ -39,8 +42,6 @@ def eigenvector_corresponding_to_maximal_eigenvalue(matrix):
     eigenvalues, eigenvectors = eig(matrix)
     max_eigenvalue_index = np.argmax(np.abs(eigenvalues))
     return eigenvectors[:,max_eigenvalue_index]
-
-from math import sin, cos, tan
 
 class analog_QD(Quantum_dot):
     def __init__(self, exact_model, h, J, DeltaE,epsilon):
