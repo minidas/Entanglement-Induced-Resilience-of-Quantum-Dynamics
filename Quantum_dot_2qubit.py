95c1eb095a37153be35d45b0790edbe12378d67a
minidas <caoyue3004@gmail.com>
2025-11-27 20:14:45 +0800

    imp: RCP initial state

--- Quantum_dot_2qubit.py
+++ Quantum_dot_2qubit.py
@@ -18,22 +18,17 @@ from scipy.linalg import expm
 
 class Quantum_dot:
 
-    def __init__(self, Omega, J0, N=16):
-        self.Omega = Omega
+    def __init__(self,  J0, N=16):
+
         self.J0 = J0
         self.N = N
-        self.H = self.get_hamiltonian(Omega, J0, N)
+        self.H = self.get_hamiltonian(J0, N)
 
-    def get_hamiltonian(self, Omega, J0, N):
-        IZ = qt.tensor(
-            *[qt.sigmaz() if i == 0 else qt.qeye(2) for i in range(N)]
-        )+ qt.tensor(
-            *[qt.sigmaz() if i == 1 else qt.qeye(2) for i in range(N)]
-        )
+    def get_hamiltonian(self, J0, N):
         ZZ= qt.tensor(
             *[qt.sigmaz() if i in [0,1] else qt.qeye(2) for i in range(N)]
         )
-        return Omega * 0.5* IZ+ 0.25* J0 * ZZ
+        return 0.25* J0 * ZZ
     
 from random_PS import *
 num_rand_PS=2**(N-2)#number of random Pauli strings
@@ -50,7 +45,6 @@ from math import sin, cos, tan
 
 class analog_QD(Quantum_dot):
     def __init__(self, exact_model, h, J, DeltaE,epsilon):
-        self.Omega = exact_model.Omega
         self.J0 = exact_model.J0
         self.h = h
         self.J = J
@@ -87,7 +81,7 @@ class analog_QD(Quantum_dot):
                 *[qt.sigmaz() if i in [1,j] else qt.qeye(2) for i in range(self.N)]
             )
 
-        self.H = (self.get_hamiltonian(self.Omega,self.J0,self.N) 
+        self.H = (self.get_hamiltonian(self.J0,self.N) 
                   + h * IZ + 0.25 * J * ZZ)
 
     def time_dependent_coefficient_XZ(self,t):
